b4b25210b3dfc6add5b5bca5fd58e810e84ec978
Venkatesh <YOUR_GITHUB_EMAIL>
2026-07-23 13:11:34 +0530

    Add backend folder

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+employees = [
+    {"id": 1, "name": "Venkatesh", "role": "DevOps Engineer"},
+    {"id": 2, "name": "Rahul", "role": "Python Developer"},
+    {"id": 3, "name": "Priya", "role": "React Developer"}
+]
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI Backend Running"}
+
+@app.get("/employees")
+def get_employees():
+    return employees
