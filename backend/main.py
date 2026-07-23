f320bcf4fd2fc7ce6b24bf0bde9ed340ea95f578
Venkatesh <YOUR_GITHUB_EMAIL>
2026-07-23 18:26:00 +0530

    Cleaned backend code

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,18 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 employees = [
     {"id": 1, "name": "Venkatesh", "role": "DevOps Engineer"},
     {"id": 2, "name": "Rahul", "role": "Python Developer"},
-    {"id": 3, "name": "Priya", "role": "React Developer"}
+    {"id": 3, "name": "Priya", "role": "React Developer"},
 ]
+
 
 @app.get("/")
 def home():
     return {"message": "FastAPI Backend Running"}
+
 
 @app.get("/employees")
 def get_employees():
