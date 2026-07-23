851922b39b5bb31200381ad1e5fce8a7c2a39b14
Venkatesh <YOUR_GITHUB_EMAIL>
2026-07-23 18:19:50 +0530

    Fixed Flake8 formatting

--- backend/test_main.py
+++ backend/test_main.py
@@ -3,9 +3,11 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
+
 
 def test_employees():
     response = client.get("/employees")
